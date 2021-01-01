db877e7b293a23ce598f96074899386deb2a67d9
Ponsingh <ponsinghlj@gmail.com>
2021-01-01 19:00:43 +0530

    Change Git Email

--- app/src/main/java/com/ponsingh/mymap/MapsActivity.kt
+++ app/src/main/java/com/ponsingh/mymap/MapsActivity.kt
@@ -14,6 +14,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
